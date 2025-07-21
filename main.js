42ab382d65e28db3bed6309fc4e7cb0165914e1b
Pavel <chicony2753@gmail.com>
2025-07-21 16:50:05 +0300

    Changed concept

--- main.js
+++ main.js
@@ -19,74 +19,6 @@ let upgrades = {
 	}
 }
 
-let mapList = [
-	{id: 1, name: "Cen", defender: 0, reward: 0, captured: false, locked: false},
-	{id: 2, name: "Gom", defender: 0, reward: 0, captured: false, locked: false},
-	{id: 3, name: "Sam", defender: 0, reward: 0, captured: false, locked: false},
-	{id: 4, name: "Rem", defender: 0, reward: 0, captured: false, locked: false},
-	{id: 5, name: "Vok", defender: 0, reward: 0, captured: false, locked: false},
-	{id: 6, name: "Shi", defender: 0, reward: 0, captured: false, locked: false},
-	{id: 7, name: "Nin", defender: 0, reward: 0, captured: false, locked: false},
-	{id: 8, name: "Med", defender: 0, reward: 0, captured: false, locked: false},
-	{id: 9, name: "Gon", defender: 0, reward: 0, captured: false, locked: false},
-	{id: 10, name: "Nag", defender: 0, reward: 0, captured: false, locked: false},
-	{id: 11, name: "Tar", defender: 0, reward: 0, captured: false, locked: false},
-	{id: 12, name: "Zar", defender: 0, reward: 0, captured: false, locked: false},
-];
-
-let mapLocation = [
-	[0, 0, 11, 0, 0, 0, 0],
-	[8, 7, 2, 1, 3, 4, 10],
-	[0, 0, 12, 5, 6, 0, 0],
-	[0, 0, 0, 9, 0, 0, 0],
-]
-
-// Создаём карту
-function renderMap() {
-    const map = document.getElementById('clickerMap');
-    map.innerHTML = '';
-	map.style.cssText = `
-		grid-template-columns: repeat(${mapLocation[0].length}, 1fr);
-		grid-template-rows: repeat(${mapLocation.length}, 1fr);
-	`
-
-    mapLocation.forEach(row => {
-		row.forEach(item => {
-			const cell = document.createElement('div');
-
-			if(item != 0) { 
-				let el = mapList[item - 1];
-
-				cell.className = `territory ${el.captured ? 'captured' : ''} ${el.locked ? 'locked' : ''}`;
-				cell.textContent = el.name;
-        		cell.dataset.id = el.id;
-
-				if (!el.locked && !el.captured) {
-					cell.addEventListener('click', () => captureTerritory(el.id));
-				}
-			}
-
-			map.appendChild(cell);
-		})
-
-    });
-}
-
-// Захват территории
-function captureTerritory(territoryId) {
-    const territory = mapList.find(t => t.id === territoryId);
-    
-    if (stats.clicks >= territory.defender) {
-        stats.clicks -= territory.defender;
-		territory.defender += 10; 
-        territory.captured = true;
-        
-        updateCounter();
-        renderMap();
-    } else {
-        alert(`Нужно больше армии! Требуется: ${territory.defender}`);
-    }
-}
 // Загрузка сохранения при старте
 function loadGame() {
 	const savedData = localStorage.getItem('clickerSave');
@@ -99,7 +31,6 @@ function loadGame() {
 		updateCounter();
 		console.log("Игра загружена!");
 	}
-	renderMap();
 }
 
 // Сохранение игры
@@ -116,7 +47,7 @@ function saveGame() {
 
 // Обновление характеристик
 function updateStats() {
-	document.getElementById("clickerValueClick").textContent = stats.clickPower;
+	document.getElementById("clickerValueGang").textContent = stats.clickPower;
 	document.getElementById("clickerValueAutoclick").textContent = stats.autoClickers;
 	
 	document.getElementById("clickCount").textContent = `x${upgrades['click'].count}`;
